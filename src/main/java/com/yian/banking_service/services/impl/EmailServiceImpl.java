0a50d8baf9bc897aa707b32a1f7cac6b32d074c5
fjg105cdy <146270786+fjg105cdy@users.noreply.github.com>
2025-06-04 15:45:35 +0100

    Add mail set up #2 with Thymeleaf

--- src/main/java/com/yian/banking_service/services/impl/EmailServiceImpl.java
+++ src/main/java/com/yian/banking_service/services/impl/EmailServiceImpl.java
@@ -1,29 +1,69 @@
 package com.yian.banking_service.services.impl;
 
 import com.yian.banking_service.services.EmailService;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+
+import java.util.concurrent.Executor;
 
 //import java.util.concurrent.Executors;
 
 @Service
-@RequiredArgsConstructor
 @Slf4j
 public class EmailServiceImpl implements EmailService {
     private final JavaMailSender mailSender;
-//    private final Executors executors;
+    private final SpringTemplateEngine templateEngine;
+    private final Executor executor;
+
+    public EmailServiceImpl(
+            JavaMailSender mailSender,
+            SpringTemplateEngine templateEngine,
+            @Qualifier("emailExecutor") Executor executor
+    ){
+        this.mailSender = mailSender;
+        this.templateEngine = templateEngine;
+        this.executor = executor;
+    }
 
     @Override
     public void sendEmail(String to, String name) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setSubject("welcome to banking application");
-        message.setText("Welcome to banking application by Yian");
+//        SimpleMailMessage message = new SimpleMailMessage();
+//        message.setTo(to);
+//        message.setSubject("welcome to banking application");
+//        message.setText("Welcome to banking application by Yian");
+//
+//        mailSender.send(message);
 
-        mailSender.send(message);
+        executor.execute(()->{
+            try{
+
+                MimeMessage mimeMessage = mailSender.createMimeMessage();
+                MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+
+                //템플릿 html 처리
+                Context context = new Context();
+                context.setVariable("name", name);
+                String htmlContent = templateEngine.process("welcome-email",context);
+
+                helper.setTo(to);
+                helper.setSubject("Welcome to Banking Application");
+                helper.setText(htmlContent, true);
+
+                mailSender.send(mimeMessage);
+
+            } catch (Exception e){
+                System.err.println("Email send fail"+e.getMessage());
+                e.printStackTrace();
+            }
+        });
 
     }
 }
